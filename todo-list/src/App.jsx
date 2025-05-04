dc9c5953f5b651772ca7c1d1db1122ff8cc05dbd
SobiaRehman <161967184+Sobiarehman11@users.noreply.github.com>
2025-05-04 06:59:25 +0500

    dyufyg

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, ChangeEvent } from 'react';
+import './App.css';
+
+interface Task {
+  id: number;
+  name: string;
+  
+}
+
+function App() {
+  const [data, setData] = useState<Task[key]>([]);
+  const [task, setTask] = useState<string>("");
+  const [editId, setEditId] = useState<number | null>(null);
+
+  const addTask = () => {
+     if (!task.trim()) return;
+
+    if (editId !== null) {
+      const updated = data.map((t) =>
+        t.id === editId ? { ...t, name: task } : t
+      );
+      setData(updated);
+      setEditId(null);
+    } else {
+      setData([...data, { id: Date.now(), name: task }]);
+    }
+
+    setTask("");
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
+  const deleteTask = (id: number) => {
+    const filtered = data.filter((t) => t.id !== id);
+    setData(filtered);
+  };
+
+  const editTask = (id: number) => {
+    const toEdit = data.find((t) => t.id === id);
+    if (toEdit) {
+      setTask(toEdit.name); 
+      setEditId(id);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>To-Do List</h1>
+      <div className="input-wrapper">
+        <input
+          type="text"
+          value={task}
+          onChange={handleInputChange}
+          placeholder="Enter a task"
+        />
+        <button onClick={addTask}>{editId ? "Update" : "Add"}</button>
+      </div>
+
+      <ul>
+        {data.map((t) => (
+          <li key={t.id}>
+            {t.name}
+            <div className="manage-btns">
+              <button onClick={() => editTask(t.id)}>Edit</button>
+              <button onClick={() => deleteTask(t.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
